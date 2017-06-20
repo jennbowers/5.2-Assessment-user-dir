b8bb5ac81a762b160476727d5c1f6aaa1ecc0289
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-20 16:06:42 -0400

    jadded in content to directory.mustache, just got styles.css linked

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const path = require('path');
+var data = require('./data.js');
 
 const app = express();
 
+app.use(express.static('public'));
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
-app.set('view enigine', 'mustache');
+app.set('view engine', 'mustache');
 
-app.get('/directory/', function(req, res) {
+// app.get('/', function(req, res) {
+//   res.send('HELLO');
+// });
 
+app.get('/directory', function(req, res) {
+  // res.send('HELLO');
+  // console.log(data);
+
+    console.log(data);
+
+  res.render('directory', data);
 });
 
 app.listen(3000, function() {
